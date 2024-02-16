c4c121657c2961b0d9ce74745f925eafc4a70cd5
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-02-16 07:18:34 -0800

    Making BeanNameConfiguration and BeanNameTest for Bean Name test

---
+++ learning-spring-boot/src/test/java/com/learning/springboot/learningspringboot/BeanNameTest.java
@@ -0,0 +1,32 @@
+package com.learning.springboot.learningspringboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.learning.springboot.learningspringboot.data.Foo;
+
+public class BeanNameTest {
+
+    private ApplicationContext applicationContext;
+    
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(BeanNameConfiguration.class);
+    }
+
+    @Test
+    void testBeanName() {
+        Foo foo = applicationContext.getBean(Foo.class);
+        Foo foo1 = applicationContext.getBean("fooFirst", Foo.class);
+        Foo foo2 = applicationContext.getBean("fooSecond", Foo.class);
+
+        Assertions.assertSame(foo, foo1);
+        Assertions.assertNotSame(foo1, foo2);
+    }
+
+    
+
+}
