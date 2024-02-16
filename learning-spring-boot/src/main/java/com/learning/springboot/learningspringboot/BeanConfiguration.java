888dd83c3aa115c78ee9d24152a7dfdc3b6c6168
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-02-16 02:54:01 -0800

    Making file BeanTest for Bean test

--- learning-spring-boot/src/main/java/com/learning/springboot/learningspringboot/BeanConfiguration.java
+++ learning-spring-boot/src/main/java/com/learning/springboot/learningspringboot/BeanConfiguration.java
@@ -1,5 +1,20 @@
 package com.learning.springboot.learningspringboot;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.learning.springboot.learningspringboot.data.Foo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
 public class BeanConfiguration {
 
+    @Bean
+    public Foo foo(){
+        Foo foo = new Foo();
+        log.info("Create new Foo");
+        return foo;
+    }
 }
