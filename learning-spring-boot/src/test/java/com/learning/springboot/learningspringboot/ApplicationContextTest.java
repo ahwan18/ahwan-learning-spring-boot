de560af68f112b681acdeb5bdb2089433aad7b53
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-02-16 02:45:50 -0800

    Making file Database and Database test for Singleton Test and Changing Application Context Name test

--- learning-spring-boot/src/test/java/com/learning/springboot/learningspringboot/ApplicationContextTest.java
+++ learning-spring-boot/src/test/java/com/learning/springboot/learningspringboot/ApplicationContextTest.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ApplicationContextTest {
 
     @Test
-    void testName() {
+    void testAppicationContext() {
         ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         Assertions.assertNotNull(context);
