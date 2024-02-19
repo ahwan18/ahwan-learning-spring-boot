89ea83a0d4f9646caf0bfb3fa6aa1c760675df85
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-02-18 19:01:22 -0800

    Making and modifying file DependencyInjectionConfiguration and Modifying DependencyInjectionTest to test Using Dependency Injection and test choosing Dependencies using annotation Qualifier

--- learning-spring-boot/src/test/java/com/learning/springboot/learningspringboot/DependencyInjectionTest.java
+++ learning-spring-boot/src/test/java/com/learning/springboot/learningspringboot/DependencyInjectionTest.java
@@ -1,13 +1,33 @@
 package com.learning.springboot.learningspringboot;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.learning.springboot.learningspringboot.data.Bar;
 import com.learning.springboot.learningspringboot.data.Foo;
 import com.learning.springboot.learningspringboot.data.FooBar;
 
 public class DependencyInjectionTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+     applicationContext = new AnnotationConfigApplicationContext(DependencyInjectionConfiguration.class);   
+    }
+
+    @Test
+    void testDI() {
+        Foo foo = applicationContext.getBean("fooSecond", Foo.class);
+        Bar bar = applicationContext.getBean(Bar.class);
+        FooBar fooBar = applicationContext.getBean(FooBar.class);
+
+        Assertions.assertSame(foo, fooBar.getFoo());
+        Assertions.assertSame(bar, fooBar.getBar());
+    }
 
     @Test
     void testNoDI() {
@@ -20,5 +40,7 @@ public class DependencyInjectionTest {
         Assertions.assertSame(foo, fooBar.getFoo());
         Assertions.assertSame(bar, fooBar.getBar());
     }
+
+
 
 }
