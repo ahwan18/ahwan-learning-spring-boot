24916b81e3948f27b4fb10c7a793554aff972121
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-03-24 22:53:56 +0700

    Making file AppStartingListener to test using SpringApplicationEvent

--- learning-spring-boot/src/main/java/com/learning/springboot/learningspringboot/application/FooApplication.java
+++ learning-spring-boot/src/main/java/com/learning/springboot/learningspringboot/application/FooApplication.java
@@ -1,5 +1,8 @@
 package com.learning.springboot.learningspringboot.application;
 
+import java.util.List;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.learning.springboot.learningspringboot.data.Bar;
 import com.learning.springboot.learningspringboot.data.Foo;
+import com.learning.springboot.learningspringboot.listener.AppStartingListener;
 
 @SpringBootApplication
 public class FooApplication {
@@ -16,8 +20,19 @@ public class FooApplication {
         return new Foo();
     }
 
+    // public static void main(String[] args) {
+    //     ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+    //     Foo foo = applicationContext.getBean(Foo.class);
+    //     System.out.println(foo);
+    // }
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.setListeners(List.of(new AppStartingListener()));
+
+        ConfigurableApplicationContext applicationContext = application.run(args);
+
         Foo foo = applicationContext.getBean(Foo.class);
         System.out.println(foo);
     }
