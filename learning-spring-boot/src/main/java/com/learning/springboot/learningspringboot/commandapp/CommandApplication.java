d971947aa2030c3f43dc2a3959e686cad6341eb5
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-03-25 13:54:44 +0700

    Making folder command and making CommandApplicationRunner to test using CommandLineRunner implements to run arguments in launch.json

---
+++ learning-spring-boot/src/main/java/com/learning/springboot/learningspringboot/commandapp/CommandApplication.java
@@ -0,0 +1,28 @@
+package com.learning.springboot.learningspringboot.commandapp;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+public class CommandApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+    @Slf4j
+    @Component
+    public static class LogCommandLineRunner implements CommandLineRunner{
+
+        @Override
+        public void run(String... args) throws Exception {
+        log.info("Run with args = {}", Arrays.toString(args));  
+        }
+    }
+}
